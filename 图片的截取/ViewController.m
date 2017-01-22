966e92d414f499002f3957f0f83a56ec0d832c91
“shanghaijunheng” <78899500@qq.com>
2017-01-22 13:28:53 +0800

    Initial Commit

---
+++ 图片的截取/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  图片的截取
+//
+//  Created by 上海均衡 on 2017/1/22.
+//  Copyright © 2017年 上海均衡. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
